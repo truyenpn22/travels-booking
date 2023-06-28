39d6cd1844af751c6fdd3a97351ceded4aa7bbab
truyenpn22 <phantruyen19722@gmail.com>
2023-06-28 11:31:36 +0700

    Setup-file

---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../page/Home";
+import Tours from "../page/Tours";
+import TourDetails from "../page/TourDetails";
+import Login from "../page/Login";
+import Register from "../page/Register";
+import SearchResultList from "./../page/SearchResultList";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/tours" element={<Tours />} />
+      <Route path="/tours/:id" element={<TourDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/tours/search" element={<SearchResultList />} />
+    </Routes>
+  );
+};
+
+export default Routers;
