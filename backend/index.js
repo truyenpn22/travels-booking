cd8f9cd6c3b1b2c057ce7eb81ce28183e16b8941
truyenpn22 <phantruyen19722@gmail.com>
2023-07-05 10:58:37 +0700

    testing

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -43,6 +44,15 @@ app.use("/api/v1/tours", tourRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/review", reviewRoute);
 app.use("/api/v1/booking", bookRoute);
+
+var __dirname = path.resolve()
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+
+
 
 app.listen(port, () => {
   connect();
